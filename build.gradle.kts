59abc57f40f05541613cbd48568fd827e7f9df65
Ludea <ludealol.olol@gmail.com>
2022-08-28 20:22:19 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,25 +2,22 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("rustPlugin")
-    {{~#each android-app-plugins}}
-    id("{{this}}"){{/each}}
 }
 
 android {
     compileSdk = 33
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
-        applicationId = "{{reverse-domain app.domain}}.{{snake-case app.name}}"
-        minSdk = {{android.min-sdk-version}}
+        applicationId = "com.ludea.hello"
+        minSdk = 24
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
     sourceSets.getByName("main") {
-        {{#if android.vulkan-validation}}// Vulkan validation layers
+        // Vulkan validation layers
         val ndkHome = System.getenv("NDK_HOME")
         jniLibs.srcDir("${ndkHome}/sources/third_party/vulkan/src/build-android/jniLibs")
-        {{/if}}
     }
     buildTypes {
         getByName("debug") {
@@ -29,10 +26,58 @@ android {
             isJniDebuggable = true
             isMinifyEnabled = false
             packagingOptions {
-                {{~#each targets}}
-                  
-                jniLibs.keepDebugSymbols.add("*/{{this.abi}}/*.so")
-                {{/each}}
+                jniLibs.keepDebugSymbols.add("*/arm64-v8a/*.so")
+17m 52s ago
+2s
+1s
+19s
+1s
+1s
+4m 36s
+39s
+11s
+47s
+1s
+0s
+Run ls src-tauri/gen/android/hello/app
+build.gradle.kts
+proguard-rules.pro
+src
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("rustPlugin")
+}
+
+android {
+    compileSdk = 33
+    defaultConfig {
+        manifestPlaceholders["usesCleartextTraffic"] = "false"
+        applicationId = "com.ludea.hello"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+    sourceSets.getByName("main") {
+        // Vulkan validation layers
+        val ndkHome = System.getenv("NDK_HOME")
+        jniLibs.srcDir("${ndkHome}/sources/third_party/vulkan/src/build-android/jniLibs")
+    }
+    buildTypes {
+        getByName("debug") {
+            manifestPlaceholders["usesCleartextTraffic"] = "true"
+            isDebuggable = true
+            isJniDebuggable = true
+            isMinifyEnabled = false
+            packagingOptions {
+                jniLibs.keepDebugSymbols.add("*/arm64-v8a/*.so")
+
+                jniLibs.keepDebugSymbols.add("*/armeabi-v7a/*.so")
+
+                jniLibs.keepDebugSymbols.add("*/x86/*.so")
+
+                jniLibs.keepDebugSymbols.add("*/x86_64/*.so")
             }
         }
         getByName("release") {
@@ -44,55 +89,16 @@ android {
     productFlavors {
         create("universal") {
             val abiList = findProperty("abiList") as? String
+
             dimension = "abi"
             ndk {
-                abiFilters += abiList?.split(",")?.map { it.trim() } ?: listOf(
-                    {{~#each targets}}
-                    "{{this.abi}}",{{/each}}
+                abiFilters += abiList?.split(",")?.map { it.trim() } ?: listOf(                    "arm64-v8a",                    "armeabi-v7a",                    "x86",                    "x86_64",
                 )
             }
         }
-
-        {{~#each targets}}
-
-        create("{{this.arch}}") {
+        create("arm64") {
             dimension = "abi"
             ndk {
-                abiFilters += listOf("{{this.abi}}")
+                abiFilters += listOf("arm64-v8a")
             }
         }
-        {{/each}}
-    }
-
-    assetPacks += mutableSetOf({{quote-and-join-colon-prefix asset-packs}})
-}
-
-rust {
-    rootDirRel = "{{root-dir-rel}}"
-    targets = listOf({{quote-and-join target-names}})
-    arches = listOf({{quote-and-join arches}})
-}
-
-dependencies {
-    {{~#each android-app-dependencies-platform}}
-    implementation(platform("{{this}}")){{/each}}
-    {{~#each android-app-dependencies}}
-    implementation("{{this}}"){{/each}}
-    implementation("androidx.webkit:webkit:1.4.0")
-     implementation("androidx.appcompat:appcompat:1.5.0")
-    implementation("com.google.android.material:material:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-}
-
-afterEvaluate {
-    android.applicationVariants.all {
-        tasks["mergeUniversalReleaseJniLibFolders"].dependsOn(tasks["rustBuildRelease"])
-        tasks["mergeUniversalDebugJniLibFolders"].dependsOn(tasks["rustBuildDebug"])
-        productFlavors.filter{ it.name != "universal" }.forEach { _ ->
-            val archAndBuildType = name.capitalize()
-            tasks["merge${archAndBuildType}JniLibFolders"].dependsOn(tasks["rustBuild${archAndBuildType}"])
-        }
-    }
-}
