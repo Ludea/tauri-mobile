2f0e1576a73d538fa5ce4865a39f29a6a2f96486
Ludea <ludealol.olol@gmail.com>
2022-09-10 19:28:03 +0200

    Update target.rs

--- target.rs
+++ target.rs
@@ -266,8 +266,7 @@ fn compile_lib(
                     .map_err(CompileLibError::MissingTool)?
                     .display()
                     .to_string()
-                    .replace("\\", "/")
-                    .replace("\", "/");
+                    .replace("\\", "/"), 
             )
             .with_env_var(
                 "TARGET_CXX",
