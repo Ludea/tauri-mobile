355535359de041723e90dc0902e7ff02e4a26c31
Ludea <ludealol.olol@gmail.com>
2022-12-13 07:37:30 +0100

    internal-ip configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { internalIpV4 } from 'internal-ip'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(async () => {
+  const host = await internalIpV4();
+  
   plugins: [react(), tsconfigPaths()]
+ 
+  server: {
+    host: '0.0.0.0', // listen on all addresses
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host,
+      port: 5183,
+    },
+  },
 })
